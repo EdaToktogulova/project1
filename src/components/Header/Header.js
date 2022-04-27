31c7553fee70fe526de7d253b0126861eec021a2
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-27 11:03:02 +0600

    Basic styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import "./Header.css"
+import headerImage from "../../assets/header.jpeg";
 
 function Header() {
+   const style = {
+      backgroundImage: `url(${headerImage})`
+   };
+
    return (
-      <header className="Header">
-         <h1>Welcome to our perfume shop</h1>
-         <p>This is an internet shop</p>
+      <header className="Header" style={style}>
+         <div>Welcome to our</div>
+         <h1>PERFUME SHOP</h1>
+         <p>The perfume is defined on paper as a mixture of <br/> fragrant essential oils or aroma compounds usually <br/> in liquid form... but it is so much more than that: <br/> beautiful and inspiring, it is an authentic object of <br/> desire, and part of our identity. <br/>
+         Find here the most <br/> iconic perfumes and the most delightful and desired fragrances.</p>
       </header>
    );
 }

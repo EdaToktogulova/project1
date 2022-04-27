178181fc242c9bc19db49398cdc604c57c5e1b2d
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-27 10:38:52 +0600

    Add new component NavItem

---
+++ src/components/Nav/NavItem/NavItem.js
@@ -0,0 +1,59 @@
+export const NavItems = [
+   {
+      id: 1,
+      title: "Home",
+      path: "/",
+      cName: "nav-item"
+   },
+   {
+      id: 2,
+      title: "Top",
+      path: "/top",
+      cName: "nav-item"
+   },
+   {
+      id: 3,
+      title: "Discounts",
+      path: "/discounts",
+      cName: "nav-item"
+   },
+   {
+      id: 4,
+      title: "Novelty",
+      path: "/novelty",
+      cName: "nav-item"
+   },
+   {
+      id: 5,
+      title: "About",
+      path: "/about",
+      cName: "nav-item"
+   },
+   {
+      id: 6,
+      title: "Category",
+      path: "/category",
+      cName: "nav-item"
+   },
+   {
+      id: 7,
+      title: "Catalog",
+      path: "/catalog",
+      cName: "nav-item"
+   }
+];
+
+export const serviceDropdown = [
+   {
+      id: 1,
+      title: "About store",
+      path: "/about store",
+      cName: "submenu-item"
+   },
+   {
+      id: 2,
+      title: "Contacts",
+      path: "/contacts",
+      cName: "submenu-item"
+   }
+];

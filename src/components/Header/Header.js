4a13e53de7db228d0e2ba048d314b8f741c92d2c
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-04-27 10:37:57 +0600

    Add new component Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header() {
    return (
-      <header>
+      <header className="Header">
          <h1>Welcome to our perfume shop</h1>
          <p>This is an internet shop</p>
       </header>

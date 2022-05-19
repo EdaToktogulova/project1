3db87351c0f1fee968e02f06c78414a627b6b482
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-19 15:30:36 +0600

    New component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+   return (
+      <div>
+         <header className="header">
+            <img src="https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium" />
+         </header>
+      </div>
+   )
+}
+
+export default Header;
